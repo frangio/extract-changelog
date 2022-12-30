ee664ced9b38cb586746800367e154971b88871a
Francisco Giordano <frangio.1@gmail.com>
2022-12-30 12:36:45 -0300

    fix word regexp

--- src/utils/word-regexp.ts
+++ src/utils/word-regexp.ts
@@ -2,6 +2,6 @@ export function makeWordRegExp(word: string) {
   const start = word.length > 0 && /\b/.test(word[0]!) ? '\\b' : '';
   const end = word.length > 0 && /\b/.test(word[word.length - 1]!) ? '\\b' : '';
   return new RegExp(
-    start + [...word].map(c => /[a-z]/i.test(c) ? c : '\\' + c).join('') + end
+    start + [...word].map(c => /[a-z0-9]/i.test(c) ? c : '\\' + c).join('') + end
   );
 }
