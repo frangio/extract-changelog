f9506aebb734f569ad1742df3891dc9a7584b246
Francisco Giordano <frangio.1@gmail.com>
2022-12-30 12:52:14 -0300

    place output in temp file as action output

--- src/action.ts
+++ src/action.ts
@@ -1,16 +1,24 @@
 import * as core from '@actions/core';
 import * as github from '@actions/github';
 import { extractSection } from './core';
+import { promises as fs } from 'fs';
+import path from 'path';
 
 (async () => {
   const inputFile = core.getInput('changelog');
-  const outputFile = core.getInput('output');
+  const input = await fs.readFile(inputFile, 'utf8');
+
+  const outputFile = path.join(await fs.mkdtemp('extract-changelog-'), 'output.md');
+
   const versionMatch = github.context.ref.match(/^refs\/tags\/v(\d[^-]*)/);
+
   if (!versionMatch) {
     throw Error('github.context.ref is not a tag');
   } else {
     const version = versionMatch[1]!;
-    await extractSection(inputFile, outputFile, version);
+    const output = await extractSection(input, version);
+    await fs.writeFile(outputFile, output);
+    core.setOutput('file', outputFile);
   }
 })().catch(e => {
   console.error(e);
--- src/core.ts
+++ src/core.ts
@@ -1,10 +1,8 @@
-import { promises as fs } from 'fs';
 import { marked } from 'marked';
 import { makeWordRegExp } from './utils/word-regexp';
 
-export async function extractSection(inputFile: string, outputFile: string, heading: string) {
-  const contents = await fs.readFile(inputFile, 'utf8');
-  const tokens = marked.lexer(contents);
+export async function extractSection(document: string, heading: string) {
+  const tokens = marked.lexer(document);
   const headingRe = makeWordRegExp(heading);
   const start = tokens.findIndex(t => t.type === 'heading' && t.text.search(headingRe) === 0);
   if (start === -1) {
@@ -13,8 +11,6 @@ export async function extractSection(inputFile: string, outputFile: string, head
     const { depth } = tokens[start]! as marked.Tokens.Heading;
     const end = tokens.findIndex((t, i) => i > start && t.type === 'heading' && t.depth <= depth);
     const section = tokens.slice(start + 1, end >= 0 ? end : undefined).map(t => t.raw).join('');
-    await fs.writeFile(outputFile, section);
+    return section;
   }
 }
-
-
