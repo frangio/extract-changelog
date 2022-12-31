3610fb39031d03f294aecacd1d6b6f855ada5e26
Francisco Giordano <frangio.1@gmail.com>
2022-12-31 01:04:17 -0300

    replace marked with a regexp

--- src/core.ts
+++ src/core.ts
@@ -1,14 +1,25 @@
-import { marked } from 'marked';
 import { makeWordRegExp } from './utils/word-regexp';
 
-export function extractSection(document: string, heading: string) {
-  const tokens = marked.lexer(document);
-  const headingRe = makeWordRegExp(heading);
-  const start = tokens.findIndex(t => t.type === 'heading' && t.text.search(headingRe) === 0);
-  if (start >= 0) {
-    const { depth } = tokens[start]! as marked.Tokens.Heading;
-    const end = tokens.findIndex((t, i) => i > start && t.type === 'heading' && t.depth <= depth);
-    const section = tokens.slice(start + 1, end >= 0 ? end : undefined).map(t => t.raw).join('');
-    return section;
+export function extractSection(document: string, needle: string) {
+  // ATX Headings as defined in GitHub Flavored Markdown (https://github.github.com/gfm/#atx-headings)
+  const heading = /^ {0,3}(?<lead>#{1,6})(?: [ \t\v\f]*(?<text>.*?)[ \t\v\f]*)?(?:[\n\r]+|$)/mg;
+
+  const needleRe = makeWordRegExp(needle);
+
+  let start, end;
+
+  for (const m of document.matchAll(heading)) {
+    if (!start) {
+      if (m.groups!.text!.search(needleRe) === 0) {
+        start = m;
+      }
+    } else if (m.groups!.lead!.length <= start.groups!.lead!.length) {
+      end = m;
+      break;
+    }
+  }
+
+  if (start) {
+    return document.slice(start.index! + start[0].length, end?.index);
   }
 }
