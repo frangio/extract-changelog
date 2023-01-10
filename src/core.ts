7373e47f3d35cd4ecf15d32f10ca57fa2035bc33
Francisco Giordano <frangio.1@gmail.com>
2023-01-10 18:45:28 -0300

    rename variable

--- src/core.ts
+++ src/core.ts
@@ -1,16 +1,16 @@
 import { makeWordRegExp } from './utils/word-regexp';
 
-export function extractSection(document: string, needle: string) {
+export function extractSection(document: string, wantedHeading: string) {
   // ATX Headings as defined in GitHub Flavored Markdown (https://github.github.com/gfm/#atx-headings)
   const heading = /^ {0,3}(?<lead>#{1,6})(?: [ \t\v\f]*(?<text>.*?)[ \t\v\f]*)?(?:[\n\r]+|$)/mg;
 
-  const needleRe = makeWordRegExp(needle);
+  const wantedHeadingRe = makeWordRegExp(wantedHeading);
 
   let start, end;
 
   for (const m of document.matchAll(heading)) {
     if (!start) {
-      if (m.groups!.text!.search(needleRe) === 0) {
+      if (m.groups!.text!.search(wantedHeadingRe) === 0) {
         start = m;
       }
     } else if (m.groups!.lead!.length <= start.groups!.lead!.length) {
